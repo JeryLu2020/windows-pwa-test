474e2c6fca79781883c80014d0e1890c687dbb28
Jery Lu (CSS) <v-jelu@microsoft.com>
2019-04-30 17:09:02 +0800

    local working fine

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -13,7 +13,7 @@ self.addEventListener("install", function (event) {
     caches.open(CACHE).then(function (cache) {
       console.log("[PWA Builder] Cached offline page during install");
 
-      // if (offlineFallbackPage === "offline.html") {
+      // if (offlineFallbackPage == "/offline.html") {
       //   return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
       // }
       console.log("[PWA Builder] offlineFallbackPage is: " + offlineFallbackPage);
