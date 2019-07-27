d0efc69ecfdd30efdcf9681bfaf476f59b25c43e
13127755587 <13127755587@163.com>
2019-07-28 03:13:23 +0800

    add react as client

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// add React
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+// remove original express view
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // use session to store the user info
 app.use(session({
