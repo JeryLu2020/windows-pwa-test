006c8e47fbd0ff81373d874ff821b0b0a38540b9
Jery Lu <591318206@qq.com>
2019-11-25 08:52:00 +0800

    find user by email

---
+++ FunctionApp/findonebyEmail.js
@@ -0,0 +1,44 @@
+const mongodb = require("mongodb");
+const mongourl = process.env.MONGODB_URL;
+const dbName = "React";
+const collectionName = "heros";
+var ObjectId = require('mongodb').ObjectID;
+
+let client = null;
+
+module.exports = function (context, req) {
+    context.log('Running');
+
+    let hasClient = client != null;
+
+    if(client == null){
+        mongodb.MongoClient.connect(mongourl, function (error, _client) {
+            if (error) {
+                context.log('Failed to connect');
+                context.res = { status: 500, body: res.stack }
+                return context.done();
+            }
+            client = _client;
+            context.log('Connected');
+            FindOneUser(context, req.body.email);
+        });
+    } else {
+        FindOneUser(context, req.body.email);
+    }
+
+    function FindOneUser(context, email){
+        context.log('Finding....' + email);
+
+        client.db(dbName).collection(collectionName).find({"email" : email}, (err, data)=>{
+            if(data == null || err) {
+                context.res = { status: 404, body: "User doesn't exist" }
+                context.done();
+            } else {
+                context.log(data);
+                context.res = { status: 200, body: "User exist" }
+                context.done();
+            }
+            
+        })
+    }
+};
