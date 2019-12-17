12d1d14be11dab9acd62ceb2bfc61d9f554f4235
Jery Lu <591318206@qq.com>
2019-12-18 00:02:33 +0800

    modify create

---
+++ client/build/precache-manifest.6430d4073028cad921774204ca079d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac9a63147c11d181e7800e34acc98cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b83660e7373b9be980d3",
+    "url": "/static/css/2.ca6af248.chunk.css"
+  },
+  {
+    "revision": "cd2b12004dd8721ffd65",
+    "url": "/static/css/main.abfab5ce.chunk.css"
+  },
+  {
+    "revision": "b83660e7373b9be980d3",
+    "url": "/static/js/2.426f4034.chunk.js"
+  },
+  {
+    "revision": "cd2b12004dd8721ffd65",
+    "url": "/static/js/main.0fab9258.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
